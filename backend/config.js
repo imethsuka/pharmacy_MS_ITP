d1b8ebff603c3eb7e8855a7bd8b250bcc609dce4
Imeth Methsuka <109576594+imethsuka@users.noreply.github.com>
2025-03-09 17:42:35 +0530

    upadated 2.0

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://root:root@sethpharm.tlnkh.mongodb.net/test';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
