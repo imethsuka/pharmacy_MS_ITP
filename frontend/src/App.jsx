79492329bc8798f7bf1c34524f0892b2d60d0bd7
imethmethsuka <saimethmethsuka@gmail.com>
2025-03-13 19:54:01 +0530

    After Modifying App.jsx after Adding Inventory Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,14 @@ import ShowBook from './pages/ShowBook';
 import EditBook from './pages/EditBook';
 import DeleteBook from './pages/DeleteBook';
 
+// Import Inventory Pages
+import Dashboard from './pages/Inventory/Dashboard';
+import MedicineLists from './pages/Inventory/MedicineLists';
+import MedicineGroups from './pages/Inventory/MedicineGroups';
+import Reports from './pages/Inventory/Reports';
+import Notifications from './pages/Inventory/Notifications';
+
+
 const App = () => {
   return (
     <Routes>
@@ -15,6 +23,14 @@ const App = () => {
       <Route path='/books/details/:id' element={<ShowBook />} />
       <Route path='/books/edit/:id' element={<EditBook />} />
       <Route path='/books/delete/:id' element={<DeleteBook />} />
+
+      {/* Inventory Pages */}
+      <Route path='/inventory/dashboard' element={<Dashboard />} />
+      <Route path='/inventory/medicinelists' element={<MedicineLists />} />
+      <Route path='/inventory/medicinegroups' element={<MedicineGroups />} />
+      <Route path='/inventory/reports' element={<Reports />} />
+      <Route path='/inventory/notifications' element={<Notifications />} />
+
     </Routes>
   );
 };
